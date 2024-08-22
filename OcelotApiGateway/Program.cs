2663a054d2ba012f48434ea65473278c6d03aed5
Lenin Ochoa <l.ochoa@synios.com>
2024-08-22 09:49:25 +0200

    Integrate Swagger support for Ocelot API Gateway
    Added configuration for Swagger integration in the Ocelot API Gateway. Updated `Program.cs` and created several route configuration files for article and writer APIs. Implemented an upstream alteration feature in `AlterUpstream.cs` to modify Swagger JSON dynamically.

--- OcelotApiGateway/Program.cs
+++ OcelotApiGateway/Program.cs
@@ -1,20 +1,31 @@
+using MMLib.SwaggerForOcelot.DependencyInjection;
 using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
+using Ocelot.Provider.Polly;
+using OcelotApiGateway.Config;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var routes = "Routes";
+
+builder.Configuration.AddOcelotWithSwaggerSupport(options =>
+{
+    options.Folder = routes;
+});
+
+builder.Services.AddOcelot(builder.Configuration).AddPolly();
+builder.Services.AddSwaggerForOcelot(builder.Configuration);
+
+var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
+    .AddOcelot(routes, builder.Environment)
+    .AddEnvironmentVariables();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
-builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
-builder.Services.AddOcelot(builder.Configuration)
-    .AddCacheManager(x =>
-    {
-        x.WithDictionaryHandle();
-    });
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -25,6 +36,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 app.UseHttpsRedirection();
-await app.UseOcelot();
+app.UseSwaggerForOcelotUI(options =>
+{
+    options.PathToSwaggerGenerator = "/swagger/docs";
+    options.ReConfigureUpstreamSwaggerJson = AlterUpstream.AlterUpstreamSwaggerJson;
+
+}).UseOcelot().Wait();
 
 app.Run();
