9a976675ee6bdd6e3ad68a88c291924d667ebcf6
Lenin Ochoa <l.ochoa@synios.com>
2024-08-22 12:10:48 +0200

    Remove AlterUpstream and update Ocelot routes
    Removed the AlterUpstream class and replaced Polly with AppConfiguration. Updated downstream and upstream path templates and reconfigured Swagger endpoints. Simplified and consolidated the Ocelot configuration.

--- OcelotApiGateway/Program.cs
+++ OcelotApiGateway/Program.cs
@@ -1,9 +1,7 @@
+using MMLib.Ocelot.Provider.AppConfiguration;
 using MMLib.SwaggerForOcelot.DependencyInjection;
-using Ocelot.Cache.CacheManager;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
-using Ocelot.Provider.Polly;
-using OcelotApiGateway.Config;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,13 +12,8 @@ builder.Configuration.AddOcelotWithSwaggerSupport(options =>
     options.Folder = routes;
 });
 
-builder.Services.AddOcelot(builder.Configuration).AddPolly();
+builder.Services.AddOcelot(builder.Configuration).AddAppConfiguration();
 builder.Services.AddSwaggerForOcelot(builder.Configuration);
-
-var environment = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
-builder.Configuration.SetBasePath(Directory.GetCurrentDirectory())
-    .AddOcelot(routes, builder.Environment)
-    .AddEnvironmentVariables();
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
@@ -29,18 +22,14 @@ builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-app.UseHttpsRedirection();
 app.UseSwaggerForOcelotUI(options =>
 {
     options.PathToSwaggerGenerator = "/swagger/docs";
-    options.ReConfigureUpstreamSwaggerJson = AlterUpstream.AlterUpstreamSwaggerJson;
 
-}).UseOcelot().Wait();
+});
+app.UseWebSockets();
+app.UseOcelot().Wait();
+
+app.UseHttpsRedirection();
 
 app.Run();
